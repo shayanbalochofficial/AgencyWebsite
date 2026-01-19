7ecf9378f04f82e264d1ea806d51695937b5b7cc
Shayan Baloch <shayanbalochofficial@gmail.com>
2026-01-19 20:32:16 +0500

    Building Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/header";
 import React from "react";
+import Hero from "./../components/hero";
 
 function page() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ components/hero.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function Hero() {
+  return (
+    <section className="bg-linear-to-b from-white via-blue-50/30 to-white py-12 md:py-20 lg:py-28">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-20 items-center">
+          <div className="text-center md:text-left">
+            <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-900 leading-tight">
+              Lessons and insights
+              <br />
+              <span className="text-green-600">from 8 years</span>
+            </h1>
+
+            <p className="mt-6 text-lg md:text-xl text-gray-600 max-w-lg mx-auto md:mx-0">
+              Where to grow your business as a photographer: site or social
+              media?
+            </p>
+
+            <Button className="mt-10 bg-green-500 hover:bg-green-600 text-white px-8 py-6 text-base md:text-lg font-medium rounded-md shadow-md transition">
+              Register
+            </Button>
+          </div>
+
+         
+        </div>
+
+      
+      </div>
+    </section>
+  );
+}
